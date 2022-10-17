679a722dfe5d9845c337a730506fa0dc1254ac96
janis-petronis-visma <janis.petronis@visma.com>
2022-10-17 10:10:32 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <span className="vismaicon vismaicon-print vismaicon-lg">Icon here</span>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
